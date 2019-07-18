3fd8ed5cdd790993662cdf7d460bea74e3055b39
Shrikant S. Channe <shrikant.channe@gmail.com>
2019-07-18 16:05:26 +0530

    Update Random_walk_Mean_&_Error.py

--- Random_walk_Mean_&_Error.py
+++ Random_walk_Mean_&_Error.py
@@ -11,8 +11,8 @@ Nsample=100
 D=[]
 D2=[]
 for t in range(Nsample):
-Xfinal=[]
-Xfinal2=[]
+  Xfinal=[]
+  Xfinal2=[]
 for i in range(Nw):
   Xi=0
   Xf=sum(np.random.uniform(-1,1,Ns))
@@ -22,8 +22,8 @@ D.append(((sum(Xfinal2)/Nw)-(sum(Xfinal)/Nw)**2)/(2*Ns))
 D2.append((((sum(Xfinal2)/Nw)-(sum(Xfinal)/Nw)**2)/(2*Ns))**2)
 
 plt.hist(D, bins='auto')
-xlabel('Diffusion')
-ylabel('Frequency')
+plt.xlabel('Diffusion')
+plt.ylabel('Frequency')
 plt.show()
 Mean = (sum(D))/Nsample
 Error = sqrt((((sum(D2)/Nw)-(sum(D)/Nw)**2)))/sqrt(Nsample)
