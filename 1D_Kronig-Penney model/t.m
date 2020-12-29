a7c19194b9eae46e7b3115bf84139060f8a7d7b8
Shrikant S. Channe <shrikant.channe@gmail.com>
2020-12-29 21:13:44 +0530

    Add files via upload
    1D_Kronig-Penney-model

---
+++ 1D_Kronig-Penney model/t.m
@@ -0,0 +1,30 @@
+%https://www.youtube.com/watch?v=u1Rh09t_Ln8&ab_channel=Scholar-Hub
+clc;
+clear all;
+close all;
+
+L = 20; %str2duble(a1);
+n =  2 ; %strdouble(a2);
+
+k = n*pi/L;
+x = 0:0.1:L;
+
+for m=1:n
+  for mm=1:length(x) %Loop to find the probability function
+  psi(mm) = sqrt(2/L)*sin(m*pi*x(mm)/L);
+  psi2(mm) = psi(mm)^2;
+  end
+  
+  figure(1),plot(x,psi,'linewidth',2); hold on;
+  ylabel('\psi(x)','fontsize',20);
+  set(gca,'ystick,[]')
+  xlabel('Position');
+  legend('First mode','Second mode','Third mode','Best');
+
+  figure(2),plot(x,psi2,'linewidth',2); hold on;
+  ylabel('\psi^2(x)','fontsize',20);
+  set(gca,'ystick,[]')
+  xlabel('Position');
+  legend('First mode','Second mode','Third mode','Best');
+  
+end
