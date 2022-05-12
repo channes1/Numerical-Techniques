4853a3d9351b8e2e89704b7dfa8c86a6ba502cf8
Shrikant S. Channe <shrikant.channe@gmail.com>
2022-05-12 14:22:17 +0530

    Update jj_bessel.m

--- Bessel_function/jj_bessel.m
+++ Bessel_function/jj_bessel.m
@@ -1,4 +1,7 @@
- 
+% Function to write Bessel function of first kind 
+% J_n(x) = \sum^{\infty}_{m=0} \frac{(-1)^m}{m! \ (n+m)!} \left( \frac{x}{2}\right)^{n+2m}
+
+
 function jn = jj_bessel(n,x)
  sum = 0.0;
 for m = 0:25
