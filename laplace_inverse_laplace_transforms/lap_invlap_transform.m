ac92b0aa0bdac5c81e10aab8b23393a0e7b7c9fd
Shrikant S. Channe <shrikant.channe@gmail.com>
2022-06-25 15:32:25 +0530

    Add files via upload

--- laplace_inverse_laplace_transforms/lap_invlap_transform.m
+++ laplace_inverse_laplace_transforms/lap_invlap_transform.m
@@ -6,7 +6,7 @@
 %Output is a function of o
 
 %Ex. 1:
-syms t, s; 
+syms t; syms s; 
 laplace(t^3, t, s)
 
 % o/p = 6/s^4
@@ -18,7 +18,7 @@ laplace(exp(-b*t),t,s)
 %o/p = 1/(b+s)
  
 %Inverse laplace transform
-%ilaplace(yes,t)
+%ilaplace(y,t)
 %where y is a function of s
 %Result is a function of t
 
